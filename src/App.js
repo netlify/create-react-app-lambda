72a7ceb87e4f9aad14bee2bd6b4ebe1b4c02ef13
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2017-11-13 00:53:48 -0800

    Add demo of calling a lambda

--- src/App.js
+++ src/App.js
@@ -2,6 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class LambdaDemo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {loading: false, msg: null};
+  }
+
+  handleClick = (e) => {
+    e.preventDefault();
+
+    this.setState({loading: true});
+    fetch('/.netlify/functions/hello')
+      .then(response => response.json())
+      .then(json => this.setState({loading: false, msg: json.msg}));
+  }
+
+  render() {
+    const {loading, msg} = this.state;
+
+    return <p>
+      <button onClick={this.handleClick}>{loading ? 'Loading...' : 'Call Lambda'}</button><br/>
+      <span>{msg}</span>
+    </p>
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +38,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <LambdaDemo/>
       </div>
     );
   }
