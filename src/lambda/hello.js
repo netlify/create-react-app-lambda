ed3e6ddb19a0ee53b83c337dcbefc53dd3ee2792
sw-yx <swyx@swyx.io>
2018-10-18 21:07:50 -0400

    fix console logs

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -3,9 +3,7 @@ const obj = {
   foo: 'bar'
 };
 export function handler(event, context, callback) {
-  console.log('qsp', event.queryStringParameters);
-  console.log('**********');
-  console.log('mvsp', event.multiValueQueryStringParameters);
+  console.log('queryStringParameters', event.queryStringParameters);
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({ msg: 'Hello, World!', ...obj })
