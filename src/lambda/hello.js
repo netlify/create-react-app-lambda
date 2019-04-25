f4d21cffc24d208a1088c7c222b69617bb306004
sw-yx <swyx@dontemail.me>
2019-04-25 11:09:58 -0400

    remove serve reload

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,11 +1,7 @@
-// show object spread works, i.e. babel works
-const obj = {
-  foo: 'bar'
-};
 export function handler(event, context, callback) {
-  console.log('queryStringParameters', event.queryStringParameters);
+  console.log("queryStringParameters", event.queryStringParameters)
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello, World!', ...obj })
-  });
+    body: JSON.stringify({ msg: "Hello, World!" })
+  })
 }
