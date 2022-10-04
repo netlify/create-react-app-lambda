7f512304aa836e23b026a2d9c94e8d2d0dacae8e
kirbyasdf <augustuskirby@protonmail.com>
2022-10-04 19:41:58 -0400

    updated 20221004

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,8 +1,9 @@
 // this uses the callback syntax, however, we encourage you to try the async/await syntax shown in async-dadjoke.js
-export function handler(event, context, callback) {
-  console.log('queryStringParameters', event.queryStringParameters)
+
+exports.handler = (event, context, callback) => {
+  console.log("queryStringParameters", event.queryStringParameters);
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello, World!' }),
-  })
-}
+    body: JSON.stringify({ msg: "Hello, World!" }),
+  });
+};
