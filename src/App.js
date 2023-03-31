01634bcf75e6c11dce7cf8bc5dcc3c201cbc8cb5
Oluwole Eesuola <30560826+ktech2016@users.noreply.github.com>
2023-03-31 23:31:37 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
+            Saved as Edited on Github
           </p>
           <LambdaDemo />
         </header>
